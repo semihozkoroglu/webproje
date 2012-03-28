80ad218b4e5c4d64e59c38dfce60a28ce5653dec
sem <semih.ozkoroglu@bil.omu.edu.tr>
2012-03-28 22:13:14 +0300

    düzenle

--- app/models/kul.rb
+++ app/models/kul.rb
@@ -1,7 +1,7 @@
 class Kul < ActiveRecord::Base
 	def self.create_with_omniauth(auth)
 	  create! do |user|
-  	  	user.provider = auth["provider"]
+  	    user.provider = auth["provider"]
 	    user.uid = auth["uid"]
 	    user.name = auth["info"]["name"]
   	end
